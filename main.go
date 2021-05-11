3dc706263d28c84095a22074f59891b7ff0053ca
shyshlakov <shyshlakov@icloud.com>
2021-05-11 23:22:29 +0300

    feat: added new words, numbers for each user

--- main.go
+++ main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"bufio"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
+
+var usersCount map[string]int = map[string]int{
+	"shyshlakov":                  50,
+	"gore_gorchica":               50,
+	"beolnix":                     50,
+	"juleswithoutrules":           50,
+	"AlenaBetm":                   50,
+	"X_tar":                       50,
+	"andrey_ya_huy_sredi_blyadey": 50,
+	"gdgood":                      50,
+	"gleb28":                      50,
+	"Ultramarchick":               50,
+	"Pesocha":                     50,
+	"MaxLyuchin":                  50,
+}
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI("put_your_token_here")
@@ -17,8 +34,6 @@ func main() {
 	bot.Debug = true
 	quotes := readfile()
 	words := readAndrfile()
-	quoteIndex := 0
-	wordIndex := 0
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -34,33 +49,34 @@ func main() {
 
 		if update.Message.Text == "/телка" {
 			quote := ""
-			quoteIndex, quote = getNextQuote(quotes, quoteIndex)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, quote)
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
-		}
-		if update.Message.From.UserName == "andrey_ya_huy_sredi_blyadey" ||
-			update.Message.From.UserName == "@andrey_ya_huy_sredi_blyadey" ||
-			update.Message.From.UserName == "gleb28" ||
-			update.Message.From.UserName == "@gleb28" {
-			quote := ""
-			quoteIndex, quote = getNextQuote(words, wordIndex)
+			quote = getNextQuote(quotes, len(quotes)-1)
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, quote)
 			msg.ReplyToMessageID = update.Message.MessageID
 			bot.Send(msg)
 		}
 
+		if usersCount[update.Message.From.UserName] == 0 {
+			usersCount[update.Message.From.UserName] = 50
+			rand.Seed(time.Now().UnixNano())
+			wordIndex := random(0, len(words)-1)
+			quote := ""
+			quote = getNextQuote(words, wordIndex)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, quote)
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+		} else {
+			usersCount[update.Message.From.UserName] = usersCount[update.Message.From.UserName] - 1
+		}
 	}
 }
 
-func getNextQuote(quotes []string, quoteIndex int) (int, string) {
+func getNextQuote(quotes []string, quoteIndex int) string {
 	if quoteIndex >= len(quotes)-1 {
 		quoteIndex = 0
 	} else {
 		quoteIndex = quoteIndex + 1
 	}
-
-	return quoteIndex, quotes[quoteIndex]
+	return quotes[quoteIndex]
 }
 
 func readfile() []string {
@@ -101,4 +117,8 @@ func readAndrfile() []string {
 	}
 
 	return buff
+}
+
+func random(min int, max int) int {
+	return rand.Intn(max-min) + min
 }
