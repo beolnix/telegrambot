6c44cb4d4babf120d294c810d9a74ed448e7475e
beolnix <beolnix@gmail.com>
2021-05-11 11:34:29 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"os"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("put_your_token_here")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	quotes := readfile()
+	quoteIndex := 0
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		if update.Message.Text == "/телка" {
+			quote := ""
+			quoteIndex, quote = getNextQuote(quotes, quoteIndex)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, quote)
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+		}
+
+	}
+}
+
+func getNextQuote(quotes []string, quoteIndex int) (int, string) {
+	if quoteIndex >= len(quotes)-1 {
+		quoteIndex = 0
+	} else {
+		quoteIndex = quoteIndex + 1
+	}
+
+	return quoteIndex, quotes[quoteIndex]
+}
+
+func readfile() []string {
+	buff := make([]string, 0)
+	file, err := os.Open("./quotes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		buff = append(buff, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return buff
+}
